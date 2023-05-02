b89b6c5db199268942858f926b2f6268ca5b2927
Noe Perez <noeliaperez284@gmail.com>
2023-05-02 12:10:57 -0300

    funcion agregar pistas y mandar un post desde el navegador

--- client/js/pistas/pistas.js
+++ client/js/pistas/pistas.js
@@ -1,29 +1,7 @@
-const { async } = require("rxjs");
-
 let btnAgregar = document.getElementById('btnAgregar');
-let btnDuracion = document.getElementById('btnDuracion');
+// let btnDuracion = document.getElementById('btnDuracion');
 
 let pistas = [];
-
-
-
-function agregarPista() {
-    let identificador = document.getElementById('identificador').value;
-    let titulo = document.getElementById('titulo').value;
-    let duracion = document.getElementById('duracion').value;
-    let interprete = document.getElementById('interprete').value;
-
-    let nuevaPista = {
-        "id": Number(identificador),
-        "titulo": titulo,
-        "duracion": Number(duracion),
-        "interprete": interprete
-    }
-
-    pistas.push(nuevaPista);
-
-    mostrarPistas();
-}
 
 function mostrarPistas() {
     let contenedor = document.getElementById('tblPistas');
@@ -36,105 +14,124 @@ function mostrarPistas() {
             <td>${pista.titulo}</td>
            <td>${pista.duracion}</td>
            <td>${pista.interprete}</td>
+           <td>${pista.lanzamiento}</td>
         </tr>
         `
     }
-
     contenedor.innerHTML = tabla;
-
-
 }
 
-function duracion () {
-    let total = 0;
-    for(let i = 0; i < pistas.length; i++){
-        total += pistas[i].duracion;
-    }
-
-    let max = pistas[0].duracion;
-    for (let pista of pistas){
-        if(max < pista.duracion){
-            max = pista.duracion
-        }
-    } 
-
-    let contenedor_total = document.getElementById('total');
-    contenedor_total.innerHTML = 
-    `<p>Tiempo total: ${total}</p>
-    <p> Duracion maxima : ${max}</p>`;
-}
-
-async function load (){
-    
-    let promesa= fetch('mockPistas.json');
-    console.log(promesa);
-    
-    let respuesta=await promesa;
-    console.log(respuesta);
-    
-   
-    alert(respuesta.ok);
-    
-    if(respuesta.ok){
-        let t=await respuesta.json()
-        pistas=t.pistas_musicales;
-        mostrarPistas();
-
-    }
-}
-
-
-async function load2() {
-    const url_base = "http://localhost:3000";
-    const endpoint = "/pistas";
-  
-    const respuesta = await fetch(url_base + endpoint);
+async function load(){
+    const respuesta = await fetch("http://localhost:3000/pistas");
     pistas = await respuesta.json();
     console.log(pistas);
+    mostrarPistas()
+}
+
+async function agregarPista() {
     
-     mostrarPistas(pistas);
+    let titulo = document.getElementById('titulo').value;
+    let duracion = document.getElementById('duracion').value;
+    let interprete = document.getElementById('interprete').value;
+    let lanzamiento = document.getElementById('lanzamiento').value;
+
+    let nuevaPista = {
+        "titulo": titulo,
+        "duracion": Number(duracion),
+        "interprete": interprete,
+        "lanzamiento":Number(lanzamiento),
+    }
+    let resp= await aServidor(nuevaPista);
+    if(resp){ //si la funcion aServidor retorna un ok, entra en el if
+        //console.log(aServidor(nuevaPista));
+        alert("retorno true"+resp);
+        pistas.push(nuevaPista);
+        mostrarPistas();
+    } else{
+        alert("errorrrr")
+        alert("retorno false"+resp);
+        console.log(resp);
+        alert("no se pudo cargar la pista");
+    }
+
+}
 
 
-    for(let pista of pistas){
-        console.log(pista.nombre)
-        contenedor.innerHTML += `<p>${pista.titulo}</p>`
-      }
-  }
-  
+ async function aServidor(datos){
+    var resp=false;
+    try {
+    let respuesta= await fetch('/pistas', {
+        method:'POST',
+        headers:{'Content-Type': 'application/json'},
+        body: JSON.stringify(datos)
+    });
+
+    resp=  await respuesta.text()=="";
     
+    } catch (er) {
+    alert("No se pudo cargar la pista");
+    resp=false;
+}
+    return resp;
 
-//Cargar pistas desde NestJs
-// async function cargarPistas(){
-//     let respuesta= await fetch ("/pistas");
-//     let pistasNuevas= await respuesta.json();
-//     console.log(pistasNuevas)
-
-//     mostrarPistas(pistasNuevas);
-
-// }
-
-// fetch('mockPistas.json').then(respuesta => { 
-        
-// console.log(respuesta);
-// if(respuesta.ok){
-//     let t= respuesta.json()
-//     pistas=t.pistas_musicales;
-//     mostrarPistas();
-// }
-// } 
-//     );
-
-
-
-// cargarPistas();
-
-
-// load();
-load2();
-console.log(pistas)
+}
 btnAgregar.addEventListener('click', agregarPista);
-btnDuracion.addEventListener('click', duracion);
+load()
 
 
+// function duracion () {
+//     let total = 0;
+//     for(let i = 0; i < pistas.length; i++){
+//         total += pistas[i].duracion;
+//     }
+
+//     let max = pistas[0].duracion;
+//     for (let pista of pistas){
+//         if(max < pista.duracion){
+//             max = pista.duracion
+//         }
+//     } 
+
+//     let contenedor_total = document.getElementById('total');
+//     contenedor_total.innerHTML = 
+//     `<p>Tiempo total: ${total}</p>
+//     <p> Duracion maxima : ${max}</p>`;
+// }
+
+// async function load (){
+    
+//     let promesa= fetch('mockPistas.json');
+//     console.log(promesa);
+    
+//     let respuesta=await promesa;
+//     console.log(respuesta);
+    
+   
+//     alert(respuesta.ok);
+    
+//     if(respuesta.ok){
+//         let t=await respuesta.json()
+//         pistas=t.pistas_musicales;
+//         mostrarPistas();
+
+//     }
+// }
+
+//funcion cargar pista desde una archivo json desde el navegador
+// async function load2() {
+//     const url_base = "http://localhost:3000";
+//     const endpoint = "/pistas";
+  
+//     const respuesta = await fetch(url_base + endpoint);
+//     pistas = await respuesta.json();
+//     console.log(pistas);
+    
+//      mostrarPistas(pistas);
 
 
+//     for(let pista of pistas){
+//         console.log(pista.nombre)
+//         contenedor.innerHTML += `<p>${pista.titulo}</p>`
+//       }
+//   }
+  
